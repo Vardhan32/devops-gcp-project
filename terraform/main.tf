f75fcc18d1edc81cdc5f072c8d272effe910f528
Vardhan32 <harshapala23@gmail.com>
2026-09-04 18:52:17 +0530

    Add Cloud Run deployment with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,25 @@ provider "google" {
  region  = "us-central"
  service_usage_custom_endpoint = "http://localhost:4588/v1/"
   storage_custom_endpoint       = "http://localhost:4588/storage/v1/"
-
+cloud_run_v2_custom_endpoint = "http://localhost:4588/v2/"
 }
 
 resource "google_storage_bucket" "project_bucket" {
  name = "devops-gcp-project-bucket"
  location = "us"
+}
+
+resource "google_cloud_run_v2_service" "app" {
+  name     = "devops-gcp-app"
+  location = "us-central1"
+
+  template {
+    containers {
+      image = "devops-gcp-app:latest"
+
+      ports {
+        container_port = 8080
+      }
+    }
+  }
 }
