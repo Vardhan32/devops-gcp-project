ac8772ffae6c7fb6e9d472cf33f2c7c844dd27ad
Vardhan32 <harshapala23@gmail.com>
2026-09-05 09:16:46 +0530

    Simulate health check failure

--- app/app.py
+++ app/app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "DevOps GCP Project is running!"
+
+
+@app.route("/health")
+def health():
+    return {"status": "unhealthy"}, 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
